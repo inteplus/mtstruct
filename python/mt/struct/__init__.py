fb7fe138f01cb940e0647b1a7a0381a11c9d4768
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-29 19:02:54 +0100

    seems to work now

--- python/mt/struct/__init__.py
+++ python/mt/struct/__init__.py
@@ -1,3 +1,9 @@
 import mt.base.pyximportcpp as pyximport; pyximport.install(language_level=3)
 from .version import VERSION as __version__
 
+import sys
+import os.path as _op
+_dirpath = _op.dirname(__file__)
+sys.path.append(_dirpath)
+from .trie import Trie
+del sys.path[-1]
