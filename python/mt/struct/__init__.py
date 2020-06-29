12cff8b9c5e804ce256437d43ce28541120b4ae9
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-29 17:10:15 +0100

    work in progress

---
+++ python/mt/struct/__init__.py
@@ -0,0 +1,2 @@
+from .version import VERSION as __version__
+
---
+++ python/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_namespace_packages
+from mt.struct.version import VERSION
+
+setup(
+    name='mtstruct',
+    version=VERSION,
+    description="Minh-Tri's C/C++ structures that can be useful for Python.",
+    author=["Minh-Tri Pham"],
+    packages=find_namespace_packages(include=['mt.*']),
+    install_requires=[
+        'mtbase>=0.4.0',  # Minh-Tri's base modules for logging and multi-threading
+        # You need Boost.Serialization as well but only at run time.
+    ],
+    url='https://github.com/inteplus/mtstruct',
+    project_urls={
+        'Documentation': 'https://mtstruct.readthedocs.io/en/stable/',
+        'Source Code': 'https://github.com/inteplus/mtstruct',
+    }
+)
